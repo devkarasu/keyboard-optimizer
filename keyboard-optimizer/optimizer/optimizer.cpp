134d628147744f7d48936377380211ee51bb4a0f
Mashiro <devkarasu@gmail.com>
2019-01-24 11:50:04 +0900

    add GA test

--- keyboard-optimizer/optimizer/optimizer.cpp
+++ keyboard-optimizer/optimizer/optimizer.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "genetic_algorithm.h"
 
 int main(){
   std::cout << "Hello,World!" << std::endl;
+
+  ga::Generation g;
+
+  for (int i = 0; i < ga::ConstParam::GENERATION_MAX; i++) {
+    std::cout << i << std::endl;
+    g.nextGeneration();
+  }
   return 0;
 }
